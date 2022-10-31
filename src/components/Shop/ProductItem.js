47abb9eaeefd79531544c0b6f0158c6f4b007c57
ArchBoy <arakhiyarov@gmail.com>
2022-10-31 17:15:09 +0500

    added http part

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
@@ -6,10 +6,21 @@ import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
     const dispatch = useDispatch();
+
     const { title, price, description, id } = props;
 
     const addToCartHandler = () => {
-        dispatch(cartActions.addItemToCart({ id, title, price }));
+        
+        // and then send Http request
+        // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+        dispatch(
+          cartActions.addItemToCart({
+            id,
+            title,
+            price,
+          })
+        );
     };
 
     return (
